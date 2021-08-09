8da90c50263b946f5034a7e9370e34f220f561a4
lalee01 <sciro920119@gmail.com>
2021-08-09 22:54:02 +0200

    first server

---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require('express')
+const app = express()
+const mysql = require('mysql')
+const cors = require ('cors')
+
+app.use(cors())
+app.use(express.json())
+
+const db= mysql.createPool({
+    user:'root',
+    host:'localhost',
+    password:'',
+    database:'posts',
+})
+
+app.post ('/create', (req,res)=>{
+    const title = req.body.title
+    const manufacturer = req.body.manufacturer
+    const model = req.body.model
+    const description = req.body.description
+
+    db.query(
+        "INSERT INTO post (title,manufacturer,model,description) VALUES(?,?,?,?)",
+        [title,manufacturer,model,description],
+        (err,result) =>{
+            if(err) {
+                console.log(err);
+            }else {
+                res.send("Values posted")
+            }
+        }
+        
+    )
+})
+
+app.get ('/getposts', (req,res)=>{
+    const title = req.body.title
+    const manufacturer = req.body.manufacturer
+    const model = req.body.model
+    const description = req.body.description
+
+    db.query(
+        "SELECT * post (title,manufacturer,model,description) VALUES(?,?,?,?)",
+        [title,manufacturer,model,description],
+        (err,result) =>{
+            if(err) {
+                console.log(err);
+            }else {
+                res.send("Values posted")
+            }
+        }
+        
+    )
+})
+
+app.listen(3001,()=>{
+    console.log("server port is 3001")
+});
